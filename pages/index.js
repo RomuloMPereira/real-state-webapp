c7b8baf7d4af960ad37b83daa250e6c9973c60bb
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-12-29 16:20:16 -0300

    Fetching data with Axios

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+
 const Banner = ({ purpose, title1, title2, desc1, desc2, linkName, buttonText, imageUrl }) => (
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
     <Image src={imageUrl} width={500} height={300} alt="Banner" />
@@ -16,7 +18,8 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, linkName, buttonText, i
   </Flex>
 )
 
-export default function Home() {
+export default function Home({ propertiesForRent, propertiesForSale }) {
+  console.log(propertiesForRent, propertiesForSale);
   return (
     <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
       <Banner
@@ -41,4 +44,16 @@ export default function Home() {
       />
     </Box>
   )
+};
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
+  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    },
+  };
 }
